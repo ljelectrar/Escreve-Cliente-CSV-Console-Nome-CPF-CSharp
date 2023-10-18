e0b1bc539018e9baaa498dd0202c1fb2f4e21b8e
Leandro Junior Alves Dos Santos <ljelectrar@gmail.com>
2023-10-18 09:42:45 -0300

    Criado Projeto inicial e implementação dos primeiros métodos da classe AddCostumer

---
+++ ExercEscreveCSVConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using ExercEscreveCSVConsole.Services;
+using System;
+
+namespace ExercEscreveCSVConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddCustomer addCostumer = new AddCustomer();
+
+            Console.WriteLine("Insira a quantidade de dados que gostaria de inserir no vetor no seguinte formato: name, CPF");
+            
+            string userInput = Console.ReadLine();
+
+            string[] separator = userInput.Split(", ");
+
+            addCostumer.AddQtd(int.Parse(separator[0]), int.Parse(separator[1]));
+
+            Console.WriteLine(addCostumer.AddCustomerVectorSize());
+
+        }
+    }
+}
---
+++ ExercEscreveCSVConsole/Services/AddCustomer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace ExercEscreveCSVConsole.Services
+{
+    class AddCustomer
+    {
+        private string[] _costumerName;
+        private string[] _customerCPF;
+
+        private string[,] _AllDataCostumer;
+
+       
+        //Método que introduz o tamanho do vetor
+        public void AddQtd(int addQtdName, int addQtdCPF)
+        {
+            _AllDataCostumer = new string[addQtdName, addQtdCPF];
+        }
+
+        public int AddCustomerVectorSize() {
+            return _AllDataCostumer.Length; 
+        }
+
+        public void AddDataName(string name)
+        {
+            for (int i = 0; i < _costumerName.Length; i++)
+            {
+                _costumerName[i] = name;
+            }
+
+        }
+
+        public void AddDataCPF(string CPF)
+        {
+            for (int i = 0; i < _customerCPF.Length; i++)
+            {
+                _customerCPF[i] = CPF;
+            }
+        }
+
+    }
+}
